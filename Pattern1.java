5e72ebd66c1161df98c04ecbed2cc9d64f767d8c
aravinthaslan <158994475+aravinthaslan@users.noreply.github.com>
2024-02-05 11:58:18 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Pattern1
+{
+    public static void main(String args[])
+    {   
+            int i,j,n;
+        Scanner s=new Scanner(System.in);
+         n=s.nextInt();
+        for(i=0;i<=n;i++){
+            for(j=0;j<=n;j++)
+            System.out.print("*");
+            System.out.println();
+            }
+        }
+    }
