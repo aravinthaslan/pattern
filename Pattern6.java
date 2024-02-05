4760f3591bf462f0cb3186e1b70faf777677411d
aravinthaslan <158994475+aravinthaslan@users.noreply.github.com>
2024-02-05 19:21:47 +0530

    Add files via upload

---
+++ Pattern6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Pattern6
+{   
+    public static void main(String args[])
+    {
+    int i,n=4,j,k,z=1;
+    Scanner s=new Scanner(System.in);
+    for(i=1;i<=n;i++)
+    {
+        for(j=n-1;j>=i;j--)
+        {
+            System.out.print(" ");
+        }
+        for(k=1;k<=z;k++)
+        {
+            System.out.print("*");
+        }
+        z=z+2;
+         System.out.println();
+    }
+    }
+}
