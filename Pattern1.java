1040c048be5f4dc4b7b0ea99061ddcaded4dd730
aravinthaslan <158994475+aravinthaslan@users.noreply.github.com>
2024-02-05 11:59:09 +0530

    Update Pattern1.java

--- Pattern1.java
+++ Pattern1.java
@@ -3,13 +3,13 @@ class Pattern1
 {
     public static void main(String args[])
     {   
-            int i,j,n;
+        int i,j,n;
         Scanner s=new Scanner(System.in);
          n=s.nextInt();
         for(i=0;i<=n;i++){
             for(j=0;j<=n;j++)
-            System.out.print("*");
-            System.out.println();
+                System.out.print("*");
+                System.out.println();
             }
         }
     }
